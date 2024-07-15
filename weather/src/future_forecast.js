9dd40494c55fcf7cdde0ee27373bbc8e3d6e4a1f
Jacob Lin <jacoblin@Jacobs-Laptop.local>
2024-07-15 14:07:06 -0400

    added hover and active effects for the forecast rows

--- weather/src/future_forecast.js
+++ weather/src/future_forecast.js
@@ -4,11 +4,23 @@ const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Frida
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 export default function FutureForecast({forecast}) {
-    const [startIndex, setStartIndex ] = useState(0);
+    const [startIndex, setStartIndex ] = useState(0); // index of the first forecast to be displayed 
 
+    // retrieve the 4 dates to be displayed at the moment 
     const dates = []
     for (let i=startIndex; i<startIndex+4; i++){
       dates.push(new Date(forecast[i].dt * 1000));
+    }
+
+    function handlePrevious() {
+      if (startIndex > 0) {
+        setStartIndex((current) => current - 4);
+      }
+    }
+    function handleNext() {
+      if (startIndex + 4 < forecast.length){
+        setStartIndex((current) => current + 4);
+      }
     }
 
     return (
@@ -21,7 +33,7 @@ export default function FutureForecast({forecast}) {
 
         {
           dates.map((date, i) => 
-            <div className={i === 3 ? "row forecast-card-last-row" : "row"}>
+            <div className={i === 3 ? "row forecast-row forecast-card-last-row" : "row forecast-row"}>
               <div className="col-6 p-2">  
                 <h3>
                   {weekdays[date.getDay()]} 
@@ -42,17 +54,19 @@ export default function FutureForecast({forecast}) {
         }
 
         <div className='row forecast-card-button-row'>
-            <div className="col-6 p-2">
+            <div className="col-6 p-2 forecast-button" onClick={handlePrevious}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" className="bi bi-arrow-left" viewBox="0 0 16 16">
                   <path fillRule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>
                 </svg>
             </div>
-            <div className="col-6 p-2">
+            <div className="col-6 p-2 forecast-button" onClick={handleNext}>
               <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" className="bi bi-arrow-right" viewBox="0 0 16 16">
                 <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/>
               </svg>
           </div>
         </div>
       </div>
+
+
     );
   }
